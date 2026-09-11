65d36738632d5478265d52e5ea0be4c6cadec346
priyamkalra0 <priyamkalra0@gmail.com>
2026-09-11 20:26:35 +0530

    compat/sys/types: ensure `off_t` is defined

--- include/compat/sys/types.h
+++ include/compat/sys/types.h
@@ -4,7 +4,14 @@
 #ifndef _WIN32
 #include_next <sys/types.h>
 #else
+
 #include <stdint.h>
+
+#ifndef _OFF_T_DEFINED
+typedef int64_t off_t;
+#define _OFF_T_DEFINED
 #endif
+
+#endif /* WIN32 */
 
 #endif /* COMPAT_SYS_TYPES_H */
